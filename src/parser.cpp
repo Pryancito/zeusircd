8af0387d0964247c268a5ec096649227b50c2f41
Pryancito <pryan@aureaworld.info>
2018-04-03 03:28:22 +0200

    changes

--- src/parser.cpp
+++ src/parser.cpp
@@ -678,7 +678,7 @@ void Parser::parse(const std::string& message, User* user) {
 		} else {
 			ServerSet::iterator it = Servers.begin();
 			for (; it != Servers.end(); ++it)
-				user->session()->sendAsServer("002 " + user->nick() + " :Nombre: " + (*it)->name() + " IP: " + (*it)->ip() + config->EOFMessage);
+				user->session()->sendAsServer("002 " + user->nick() + " :Nombre: " + (*it)->name() + " IP: " + (*it)->ip() + " HUB: " + (*it)->uplink() + config->EOFMessage);
 			return;
 		}
 	}
