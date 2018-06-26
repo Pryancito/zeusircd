a28b121dd0df8fbac30337f1ab180d377957c3ac
Pryancito <ircd@irc.zeusircd.net>
2018-06-26 01:06:30 +0000

    v3.1-rc1

--- include/config.h
+++ include/config.h
@@ -11,7 +11,7 @@ class Config
 {
         public:
                 std::map <std::string, std::string> conf;
-                std::string version = "Zeus-3.0";
+                std::string version = "Zeus-3.1-rc1";
                 std::string file = "zeus.conf";
                 std::string EOFMessage = "\r\n";
                 std::string EOFServer = "\n";
