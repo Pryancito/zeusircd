6e363b5cec204d2b8dd0ef37878989e583eeb056
Eddie Ruiz <dev.blacknode@gmail.com>
2018-07-23 13:18:36 -0500

    delete files

--- include/utils.h
+++ include/utils.h
@@ -11,9 +11,5 @@ class Utils
 		static bool Match(const char *first, const char *second);
 		static std::string Time(time_t tiempo);
 		static bool isnumber(std::string cadena);
-<<<<<<< HEAD
-		static std::string make_string(const std::string& format, ...);
-=======
 		static std::string make_string(const std::string nickname, const std::string& fmt, ...);
->>>>>>> 908f304d1ec5e8396109a7795504e63c122e8892
 };
