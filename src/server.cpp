bda920960efdefd354932c3328d348a4c3c86df5
Pryancito <rodrigo@aureaworld.es>
2018-08-24 14:44:03 +0000

    bugfix

--- src/server.cpp
+++ src/server.cpp
@@ -17,7 +17,7 @@ Server::Server(boost::asio::io_context& io_context, std::string s_ip, int s_port
 :   mAcceptor(io_context, tcp::endpoint(boost::asio::ip::address::from_string(s_ip), s_port)), ip(s_ip), port(s_port), ssl(s_ssl), ipv6(s_ipv6)
 {
     mAcceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-    mAcceptor.listen();
+    mAcceptor.listen(1);
 }
 
 void Server::start() { 
