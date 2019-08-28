8975ea1670bd7cda849873e3470ec99042348327
Pryan <pryan@tierra.universo.int>
2019-08-28 22:33:48 +0200

    more work

--- src/base64.cpp
+++ src/base64.cpp
@@ -28,7 +28,22 @@
    René Nyffenegger rene.nyffenegger@adp-gmbh.ch
 
 */
-
+/* 
+ * This file is part of the ZeusiRCd distribution (https://github.com/Pryancito/zeusircd).
+ * Copyright (c) 2019 Rodrigo Santidrian AKA Pryan.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "base64.h"
 #include <iostream>
 
