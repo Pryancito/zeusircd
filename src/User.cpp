0e1a3bb11bc3802416d652685cba72bc1505294a
Pryan <pryan@tierra.universo.int>
2019-10-07 23:59:42 +0200

    bugfix

--- src/User.cpp
+++ src/User.cpp
@@ -146,7 +146,7 @@ void LocalUser::cmdNick(const std::string& newnick) {
     if(bSentNick) {
         if(Mainframe::instance()->changeLocalNickname(mNickName, newnick)) {
 			User::log(Utils::make_string("", "Nickname %s changes nick to: %s with ip: %s", mNickName.c_str(), newnick.c_str(), mHost.c_str()));
-            Send(mNickName + " NICK :" + newnick);
+            Send(":" + mNickName + " NICK :" + newnick);
 			Server::Send("NICK " + mNickName + " " + newnick);
 			std::string oldheader = messageHeader();
 			std::string oldnick = mNickName;
