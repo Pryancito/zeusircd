d7f63d8ecc70121576d819c3ddf60bfb83d7a7b8
Pryan <pryan@tierra.universo.int>
2019-10-16 00:47:05 +0200

    bugfix

--- src/User.cpp
+++ src/User.cpp
@@ -163,7 +163,7 @@ void LocalUser::cmdNick(const std::string& newnick) {
         }
     } else {
 		if (Mainframe::instance()->addLocalUser(this, newnick)) {
-			Send(messageHeader() + "NICK " + newnick);
+			Send(mNickName + " NICK " + newnick);
 			mNickName = newnick;
 			mCloak = sha256(mHost).substr(0, 16);
 			std::string vhost = NickServ::GetvHost(mNickName);
