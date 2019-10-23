3de4a77c42a1b742b62128794d8b1c0d4a3b11c1
Pryan <pryan@tierra.universo.int>
2019-10-23 16:22:05 +0200

    bugfix

--- src/WebUser.cpp
+++ src/WebUser.cpp
@@ -124,6 +124,6 @@ void LocalWebUser::handleRead(const boost::system::error_code &error, std::size_
 		threads.push_back(std::move(t));
 
 		read();
-	} else
-		Exit();
+	} else if (bytes == 0 || error)
+		Close();
 }
