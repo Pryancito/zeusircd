2a5c518b13e7b725735b19e03439e777e3829c8d
Pryan <pryan@tierra.universo.int>
2019-12-12 18:32:06 +0100

    v5.1-beta2

--- include/Config.h
+++ include/Config.h
@@ -26,7 +26,7 @@ class Config
 {
         public:
                 std::map <std::string, std::string> conf;
-                std::string version = "Zeus-5.1-beta";
+                std::string version = "Zeus-5.1-beta2";
                 std::string file = "zeus.conf";
 
         void Cargar ();
