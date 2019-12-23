7cb47c6a3d23e839eb30f3b357e05f6332c3efc8
Pryan <pryan@tierra.universo.int>
2019-12-23 18:19:17 +0100

    fix on check ping on websockets

--- src/WebUser.cpp
+++ src/WebUser.cpp
@@ -102,7 +102,7 @@ void LocalWebUser::check_ping(const boost::system::error_code &e)
 {
 	if (!e)
 	{
-		if (bPing + 200 < time(0) || Socket.next_layer().next_layer().is_open() == false)
+		if (bPing + 200 < time(0))
 		{
 			Close();
 		}
@@ -111,7 +111,7 @@ void LocalWebUser::check_ping(const boost::system::error_code &e)
 			LocalWebUser::Send("PING :" + config->Getvalue("serverName"));
 			deadline.cancel();
 			deadline.expires_from_now(boost::posix_time::seconds(60));
-	        deadline.async_wait(boost::bind(&LocalWebUser::check_ping, this, boost::asio::placeholders::error));
+	 		deadline.async_wait(boost::bind(&LocalWebUser::check_ping, this, boost::asio::placeholders::error));
 		}
 	}
 }
