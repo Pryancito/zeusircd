052d7d7ecdf38e44ee778ddd37faf340b5a66760
Pryan <pryan@tierra.universo.int>
2020-01-04 11:32:59 +0100

    Revert "Revert "fix""
    This reverts commit 660ab7429f7d1720bf0806a008f8afe7b8f7c6f9.

--- src/ZeusBaseClass.cpp
+++ src/ZeusBaseClass.cpp
@@ -33,9 +33,9 @@ void PublicSock::Listen(std::string ip, std::string port, bool ipv6 = false)
 	{
 		int iport = std::stoi(port);
 		size_t max = std::thread::hardware_concurrency();
-		SocketHandlerEp h();
+		StdoutLog *log = NULL;
+		SocketHandler h(log);
 		h.SetNumberOfThreads(max);
-		h.EnablePool(true);
 		ListenSocket<PlainUser> l(h);
 		l.Bind(ip, iport);
 		h.Add(&l);
