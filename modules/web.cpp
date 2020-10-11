ba11ea4c7e7da283c571c8fa87aeecfce3d22a0e
Pryan <pryan@tierra.universo.int>
2020-10-11 09:47:55 +0200

    fixes

--- modules/web.cpp
+++ modules/web.cpp
@@ -1,56 +1,75 @@
+
 #include "mainframe.h"
 #include "Config.h"
 #include "module.h"
-#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-using tcp = boost::asio::ip::tcp;
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+using namespace std;
+
+class web
+{
+public:
+  web(boost::asio::io_context& io_context,
+      const std::string& server, const std::string& path)
+  {
+        // These objects perform our I/O
+        tcp::resolver resolver(io_context);
+        beast::tcp_stream stream(io_context);
+
+        // Look up the domain name
+        auto const results = resolver.resolve(server, "80");
+
+        // Make the connection on the IP address we get from a lookup
+        stream.connect(results);
+
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, path, 11};
+        req.set(http::field::host, server);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // Send the HTTP request to the remote host
+        http::write(stream, req);
+
+        // Gracefully close the socket
+        beast::error_code ec;
+        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+  }
+};
 
 class WEB_UP : public Module
 {
   public:
 	std::thread *th;
 	boost::asio::io_context ioc;
-	boost::asio::deadline_timer deadline;
-	WEB_UP() : Module("", 50, false) , deadline(ioc.get_executor()) {
+	WEB_UP() : Module("", 50, false) {
 		th = new std::thread(&WEB_UP::init, this);
 		th->detach();
 	};
 	~WEB_UP() { close(); };
-	void send () {
-		deadline.expires_from_now(boost::posix_time::seconds(300));
-		deadline.async_wait(boost::bind(&WEB_UP::send, this));
+	void init () {
 
-		// Get a list of endpoints corresponding to the server name.
-		tcp::resolver resolver(ioc);
-		tcp::resolver::results_type endpoints = resolver.resolve("servers.zeusircd.net", "http");
-
-		// Try each endpoint until we successfully establish a connection.
-		tcp::socket socket(ioc);
-		boost::asio::connect(socket, endpoints);
-
-		// Form the request. We specify the "Connection: close" header so that the
-		// server will close the socket after transmitting the response. This will
-		// allow us to treat all data up until the EOF as the content.
-		boost::asio::streambuf request;
-		std::ostream request_stream(&request);
-		request_stream << "GET " << "/upload.php?network=" + config["network"].as<std::string>() +
-					"&server=" + config["serverName"].as<std::string>() + "&hub=" + config["hub"].as<std::string>() +
-					"&users=" + std::to_string(Mainframe::instance()->countusers()) + "&language=" + config["language"].as<std::string>() +
-					"&channels=" + std::to_string(Mainframe::instance()->countchannels()) + "&time=" + std::to_string(time(0)) + "\n" << " HTTP/1.0\r\n";
-		request_stream << "Host: " << "servers.zeusircd.net" << "\r\n";
-		request_stream << "Accept: */*\r\n";
-		request_stream << "Connection: close\r\n\r\n";
-
-		// Send the request.
-		boost::asio::write(socket, request);
-	}
-	void init ()
-	{
-		send();
-		ioc.run();
+		for(;;)
+		{
+			web c(ioc, "servers.zeusircd.net", "/upload.php?network=" + config["network"].as<std::string>() +
+				"&server=" + config["serverName"].as<std::string>() + "&hub=" + config["hub"].as<std::string>() +
+				"&users=" + std::to_string(Mainframe::instance()->countusers()) + "&language=" + config["language"].as<std::string>() +
+				"&channels=" + std::to_string(Mainframe::instance()->countchannels()) + "&time=" + std::to_string(time(0)) + "\n"
+			);
+			sleep(300);
+		}
 	}
 	void close() {
-		ioc.stop();
 		delete th;
 	}
 	virtual void command(LocalUser *user, std::string message) override {}
