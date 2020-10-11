c239bd7e5d137c372744ec3b62928a2de0713956
Pryan <pryan@tierra.universo.int>
2020-10-11 14:35:39 +0200

    fix on web.so ( not recommended use of it )

--- modules/web.cpp
+++ modules/web.cpp
@@ -1,20 +1,9 @@
-
 #include "mainframe.h"
 #include "Config.h"
 #include "module.h"
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <string>
+#include <boost/asio.hpp>
 
-namespace beast = boost::beast;     // from <boost/beast.hpp>
-namespace http = beast::http;       // from <boost/beast/http.hpp>
-namespace net = boost::asio;        // from <boost/asio.hpp>
-using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+using tcp = boost::asio::ip::tcp;
 using namespace std;
 
 class web
@@ -22,29 +11,161 @@ class web
 public:
   web(boost::asio::io_context& io_context,
       const std::string& server, const std::string& path)
+    : resolver_(io_context),
+      socket_(io_context)
   {
-        // These objects perform our I/O
-        tcp::resolver resolver(io_context);
-        beast::tcp_stream stream(io_context);
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    std::ostream request_stream(&request_);
+    request_stream << "GET " << path << " HTTP/1.0\r\n";
+    request_stream << "Host: " << server << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
 
-        // Look up the domain name
-        auto const results = resolver.resolve(server, "80");
-
-        // Make the connection on the IP address we get from a lookup
-        stream.connect(results);
-
-        // Set up an HTTP GET request message
-        http::request<http::string_body> req{http::verb::get, path, 11};
-        req.set(http::field::host, server);
-        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-        // Send the HTTP request to the remote host
-        http::write(stream, req);
-
-        // Gracefully close the socket
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+    // Start an asynchronous resolve to translate the server and service names
+    // into a list of endpoints.
+    resolver_.async_resolve(server, "http",
+        boost::bind(&web::handle_resolve, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::results));
   }
+
+private:
+  void handle_resolve(const boost::system::error_code& err,
+      const tcp::resolver::results_type& endpoints)
+  {
+    if (!err)
+    {
+      // Attempt a connection to each endpoint in the list until we
+      // successfully establish a connection.
+      boost::asio::async_connect(socket_, endpoints,
+          boost::bind(&web::handle_connect, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  void handle_connect(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // The connection was successful. Send the request.
+      boost::asio::async_write(socket_, request_,
+          boost::bind(&web::handle_write_request, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  void handle_write_request(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Read the response status line. The response_ streambuf will
+      // automatically grow to accommodate the entire line. The growth may be
+      // limited by passing a maximum size to the streambuf constructor.
+      boost::asio::async_read_until(socket_, response_, "\r\n",
+          boost::bind(&web::handle_read_status_line, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+void handle_read_status_line(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Check that response is OK.
+      std::istream response_stream(&response_);
+      std::string http_version;
+      response_stream >> http_version;
+      unsigned int status_code;
+      response_stream >> status_code;
+      std::string status_message;
+      std::getline(response_stream, status_message);
+      if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+      {
+        std::cout << "Invalid response\n";
+        return;
+      }
+      if (status_code != 200)
+      {
+        std::cout << "Response returned with status code ";
+        std::cout << status_code << "\n";
+        return;
+      }
+
+      // Read the response headers, which are terminated by a blank line.
+      boost::asio::async_read_until(socket_, response_, "\r\n\r\n",
+          boost::bind(&web::handle_read_headers, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      std::cout << "Error: " << err << "\n";
+    }
+  }
+
+  void handle_read_headers(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Process the response headers.
+      std::istream response_stream(&response_);
+      std::string header;
+      while (std::getline(response_stream, header) && header != "\r")
+        std::cout << header << "\n";
+      std::cout << "\n";
+
+      // Write whatever content we already have to output.
+      if (response_.size() > 0)
+        std::cout << &response_;
+
+      // Start reading remaining data until EOF.
+      boost::asio::async_read(socket_, response_,
+          boost::asio::transfer_at_least(1),
+          boost::bind(&web::handle_read_content, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      std::cout << "Error: " << err << "\n";
+    }
+  }
+
+  void handle_read_content(const boost::system::error_code& err)
+  {
+    if (!err)
+    {
+      // Write all of the data that has been read so far.
+      std::cout << &response_;
+
+      // Continue reading remaining data until EOF.
+      boost::asio::async_read(socket_, response_,
+          boost::asio::transfer_at_least(1),
+          boost::bind(&web::handle_read_content, this,
+            boost::asio::placeholders::error));
+    }
+    else if (err != boost::asio::error::eof)
+    {
+      std::cout << "Error: " << err << "\n";
+    }
+  }
+
+  tcp::resolver resolver_;
+  tcp::socket socket_;
+  boost::asio::streambuf request_;
+  boost::asio::streambuf response_;
 };
 
 class WEB_UP : public Module
@@ -52,13 +173,13 @@ class WEB_UP : public Module
   public:
 	std::thread *th;
 	boost::asio::io_context ioc;
-	WEB_UP() : Module("", 50, false) {
+	boost::asio::deadline_timer deadline;
+	WEB_UP() : Module("", 50, false) , deadline(ioc.get_executor()) {
 		th = new std::thread(&WEB_UP::init, this);
 		th->detach();
 	};
 	~WEB_UP() { close(); };
 	void init () {
-
 		for(;;)
 		{
 			web c(ioc, "servers.zeusircd.net", "/upload.php?network=" + config["network"].as<std::string>() +
@@ -66,10 +187,15 @@ class WEB_UP : public Module
 				"&users=" + std::to_string(Mainframe::instance()->countusers()) + "&language=" + config["language"].as<std::string>() +
 				"&channels=" + std::to_string(Mainframe::instance()->countchannels()) + "&time=" + std::to_string(time(0)) + "\n"
 			);
+			ioc.run_one();
+			ioc.run_one();
+			ioc.run_one();
 			sleep(300);
 		}
+		return;
 	}
 	void close() {
+		ioc.stop();
 		delete th;
 	}
 	virtual void command(LocalUser *user, std::string message) override {}
