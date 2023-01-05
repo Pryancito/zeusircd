7ad886e4be8c34788e2a2fa4f7ee53d83bb263b3
Pryancito <rodrigo@aureaworld.es>
2023-01-05 20:53:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ macos()
 {
 	brew update
 	for program in python cyrus-sasl ruby uuid swig wget cmake sudo; do brew install $program; done
-        qpid
 	for program in nano gcc cpp gcc-c++ openssl sqlite git gmake libicu gettext libmaxminddb mysql yaml-cpp qpid-proton; do brew install $program; done
 	sudo ln -s /usr/local/opt/openssl/include/openssl /usr/local/include
 	./configure
