9608a64a047445d72d07e6c2a0a780bc2618028c
Pryancito <rodrigo@aureaworld.es>
2023-03-23 16:10:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ qpid()
         cd qpid-proton-0.30.0
         mkdir build
         cd build
-        cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DSYSINSTALL_BINDINGS=ON -DENABLE_WARNING_ERROR=OFF -DIO2D_WITHOUT_TESTS=1
+        cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DSYSINSTALL_BINDINGS=ON -DENABLE_WARNING_ERROR=OFF
 	make
         sudo make install
         cd ../..
