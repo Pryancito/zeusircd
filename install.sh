3c8543027413a54347b54ef096de3a4da30ba029
Pryancito <rodrigo@aureaworld.es>
2023-04-07 22:19:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
 echo -e "Beginning installation"
-qpid()
-{
-	wget https://www.zeusircd.net/qpid-proton-0.38.0.tar.gz
-        tar -xzf qpid-proton-0.38.0.tar.gz
-        cd qpid-proton-0.38.0
-        mkdir build
-        cd build
-        cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DSYSINSTALL_BINDINGS=ON -DENABLE_WARNING_ERROR=OFF
-	make
-        sudo make install
-        cd ../..
-}
-
 debian()
 {
 	sudo apt-get -y update
-	sudo apt-get -y install gcc g++ cmake cmake-curses-gui uuid-dev libssl-dev libsasl2-2 libsasl2-dev libsasl2-modules swig python-dev ruby-dev wget
-	qpid
+	sudo apt-get -y install gcc g++ cmake cmake-curses-gui uuid-dev libssl-dev libsasl2-2 libsasl2-dev libsasl2-modules swig python-dev ruby-dev wget qpid-proton
 	sudo apt-get -y install nano gcc cpp g++ libssl-dev libsqlite3-dev git make gettext libicu-dev openssl libmaxminddb0 libmaxminddb-dev mmdb-bin libmariadb-dev libyaml-cpp-dev libmariadb-dev-compat
 	./configure
 	make
@@ -27,8 +13,7 @@ debian()
 ubuntu()
 {
 	sudo apt-get -y update
-	sudo apt-get -y install gcc g++ cmake cmake-curses-gui uuid-dev libssl-dev libsasl2-2 libsasl2-dev libsasl2-modules swig python-dev ruby-dev wget
-	qpid
+	sudo apt-get -y install gcc g++ cmake cmake-curses-gui uuid-dev libssl-dev libsasl2-2 libsasl2-dev libsasl2-modules swig python-dev ruby-dev wget qpid-proton
 	sudo apt-get -y install nano gcc cpp g++ libssl-dev libsqlite3-dev git make gettext libicu-dev openssl libmaxminddb0 libmaxminddb-dev mmdb-bin libmariadb-dev libyaml-cpp-dev libmariadb-dev-compat
 	./configure
 	make
@@ -38,8 +23,7 @@ redhat()
 {
 	sudo dnf group install -y "Development Tools"
 	sudo dnf install -y python3 cyrus-sasl ruby-devel uuid-devel swig wget cmake
-	qpid
-	sudo yum install wget nano gcc cpp gcc-c++ openssl-devel sqlite-devel git make libicu-devel gettext libmaxminddb-devel mariadb-client mariadb-devel yaml-cpp-devel
+	sudo yum install wget nano gcc cpp gcc-c++ openssl-devel sqlite-devel git make libicu-devel gettext libmaxminddb-devel mariadb-client mariadb-devel yaml-cpp-devel qpid-proton
 	./configure
 	make
 }
@@ -58,8 +42,7 @@ lnx()
 bsd()
 {
 	sudo pkg update
-        sudo pkg install python3 cyrus-sasl ruby uuid swig wget cmake sudo
-        qpid
+        sudo pkg install python3 cyrus-sasl ruby uuid swig wget cmake sudo qpid-proton
         sudo pkg install gcc9 openssl sqlite3 gmake gettext git bash nano libmaxminddb mariadb105-client cmake yaml-cpp
         ./configure
         gmake
@@ -68,9 +51,8 @@ bsd()
 mac()
 {
 	brew update
-	for program in python cyrus-sasl ruby uuid swig wget cmake sudo; do brew install $program; done
-	qpid
-	for program in nano gcc9 openssl sqlite git gmake gettext libmaxminddb mysql yaml-cpp boost; do brew install $program; done
+	for program in python cyrus-sasl ruby uuid swig wget cmake sudo qpid-proton; do brew install $program; done
+	for program in nano gcc9 openssl sqlite git gmake gettext libmaxminddb mysql yaml-cpp; do brew install $program; done
 	sudo ln -s /usr/local/opt/openssl/include/openssl /usr/local/include
 	./configure
 	make
